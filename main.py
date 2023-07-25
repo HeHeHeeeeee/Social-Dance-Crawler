09588e30d2e72167f5102e1237d8205777b7c2bf
wanjiepeng <1090169998@qq.com>
2023-07-25 20:12:21 +0800

    1.2

--- main.py
+++ main.py
@@ -1,39 +1,71 @@
+from threading import BoundedSemaphore
 import requests
 import os
 from concurrent.futures import ThreadPoolExecutor
 
 
 def main():
+    social_dance_download = Social_Dance_Download()
     # 指定要创建的文件夹路径
-    download_folder_path = f"./下载的歌曲"
+    download_folder_path = "下载的歌曲"
     if not os.path.exists(download_folder_path):
         # 文件夹不存在则创建
         os.makedirs(download_folder_path)
 
     # 读入音乐id列表
     id_list = []
-    music_ids_path = "./music_ids.txt"
+    music_ids_path = "music_ids.txt"
     with open(music_ids_path, "rt", encoding="utf-8") as f:
         id_list = f.readlines()
 
     # 创建线程池
     with ThreadPoolExecutor() as executor:
-        executor.map(download_mp3, id_list, download_folder_path)
+        executor.map(social_dance_download.download_mp3, id_list, [download_folder_path for _ in range(len(id_list))])
+    # for idd in id_list:
+    #     print(idd)
+    #     social_dance_download.download_mp3(id=idd, download_folder_path=download_folder_path)
 
-def download_mp3(id: int, download_folder_path: str):
-    userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42"
+class Social_Dance_Download:
 
-    api = f"https://www.dggwq.com/js/url.php?id={id}"
-    #cookie = "Hm_lvt_e6186d8b90f5a34afb05cc151292844d=1690266344; PHPSESSID=cde672thcdj8o808vjo8gm2213; Hm_lvt_3b1546f0e4ee125a0dda33500e055418=1690267035; s_id=5391; s_name=13617915869; Hm_lpvt_3b1546f0e4ee125a0dda33500e055418=1690268396; Hm_lpvt_e6186d8b90f5a34afb05cc151292844d=1690270388"
-    mp3_url = requests.get(url=api, headers={"User-Agent": userAgent})#, "Cookie": cookie})
-    print(mp3_url)
-    download_mp3 = requests.get(mp3_url).content
-    download_path = download_folder_path + mp3_url.rsplit("/", 1)[-1]
-    download_path = download_path.lsplit(".")[-1]
-    with open(download_path, "wb") as f:
-        f.write(download_mp3)
-    print(f"下载完成: {download_path}")
+    def __init__(self):
+        self.num = 1
+        self.sem = BoundedSemaphore(1)
+
+    def download_mp3(self, id, download_folder_path: str):
+        userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42"
+
+        api = f"https://www.dggwq.com/js/url.php?id={id}"
+        # 获取歌曲链接
+        mp3_url = requests.get(url=api, headers={"User-Agent": userAgent})
+        mp3_url = mp3_url.content.decode("utf-8")
+        # 下载歌曲
+        download_mp3 = requests.get(mp3_url)
+
+        # 分离出歌曲名
+        song_name = mp3_url.rsplit("/", 1)[-1]
+        song_name = song_name.split(".", maxsplit=1)[-1]
+        # print(song_name)
+
+        # 获取锁，避免文件数量更新和写入文件不同步
+        self.sem.acquire()
+        song_name = str(self.num) + "." + song_name
+        # 拼接文件路径
+        download_path = os.path.join(download_folder_path ,song_name)
+        # print(download_path)
+        # 写入文件
+        try:
+            with open(download_path, "wb") as f:
+                f.write(download_mp3.content)
+        except:
+            print("文件写入错误")
+        self.num = self.num+1
+        # print(download_path)
+        self.sem.release()
+        
+        print(f"下载完成: {song_name}")
 
 
 if __name__ == "__main__":
     main()
+    # social_dance_download = Social_Dance_Download()
+    # social_dance_download.download_mp3(63275, "./下载的歌曲")
